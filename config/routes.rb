c5ca4c333520e5522f7c51895e46756e93ddeae9
Mai Vang <vmaineng@gmail.com>
2022-07-07 08:19:28 -0700

    commit tree d9c860d35b859be8630496d17eefc032736607ea

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  post "/signup", to: "users#create"
+  get "/me", to: "users#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+
+#why make routes for users? there is a model for it
+
 end
